68e2a81b5547c3f0e482dc72a0656166cd6a71db
Abdul Jabbar <saj8180025653@gmail.com>
2024-06-08 15:06:09 +0530

    added auth & auth tests & initial Layout

---
+++ frontend/src/api-client.ts
@@ -0,0 +1,60 @@
+import { registerFormData } from "./pages/Register";
+import { SigInFormData } from "./pages/SignIn";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
+export const register = async (formData: registerFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/users/register`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData),
+    });
+    const responseBody = await response.json();
+
+    if (!response.ok){
+        throw new Error(responseBody.message);
+    }
+}
+
+export const signIn = async (formData: SigInFormData) => {    
+    const response = await fetch(`${API_BASE_URL}/api/auth/login`,{
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const body = await response.json();
+    if(!response.ok){
+        throw new Error(body.message);
+    }
+    return body;
+}
+
+export const validateToken = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`,{
+        credentials: "include"
+    })
+
+    if(!response.ok){
+        throw new Error("Token Invalid");
+    }
+
+    return response.json();
+}
+
+export const signOut = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+        credentials: "include",
+        method: "POST"
+    });
+
+    if(!response.ok){
+        throw new Error("Error During Sign Out");
+    }
+}
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,124 @@
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
+
+export type registerFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+const Register = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<registerFormData>();
+
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: async () => {
+      showToast({ message: "Registration Success!", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "This field is required" })}
+          />
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "This field is required" })}
+          />
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        />
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "Password must be Atleast 6 Characters",
+            },
+          })}
+        />
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This fiels id required";
+              } else if (watch("password") !== val) {
+                return "Your passwords do not match";
+              }
+            },
+          })}
+        />
+        {errors.confirmPassword && (
+          <span className="text-red-500">{errors.confirmPassword.message}</span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Create Account
+        </button>
+      </span>
+    </form>
+  );
+};
+export default Register;
