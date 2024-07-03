f55c81cd0702efd821c6cbc6157ea772d0d74a0a
Abdul Jabbar <saj8180025653@gmail.com>
2024-07-03 13:57:13 +0530

    edited Logo page (last comit)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <span className="text-3xl text-white font-bold tracking-tight">Holidays.com</span>
                 <span className="text-white font-bold tracking-tight flex gap-4">
-                    <p className="cursor-pointer">Privacy Policy</p>
-                    <p className="cursor-pointer">Terms of Service</p>
+                    <p className="cursor-pointer">Privacy-Policy</p>
+                    <p className="cursor-pointer">Terms of Service</p>                    
                 </span>                
             </div>
         </div>
