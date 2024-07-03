35da93beeca12ea85c2cca14b24fb4029b1ffb7f
Abdul Jabbar <saj8180025653@gmail.com>
2024-07-03 12:42:22 +0530

    added my bookings page and test

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -219,3 +219,15 @@ export const createRoomBooking = async(formData: BookingFormData) => {
     throw new Error("Error Booking room")
   }
 }
+
+export const fetchMyBookings = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-bookings`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Unable to fetch bookings");
+  }
+
+  return response.json();
+};
