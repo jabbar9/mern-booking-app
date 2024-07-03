4c260bae35b1a4b4d1323d9b72ebf42c621f2976
Abdul Jabbar <saj8180025653@gmail.com>
2024-07-03 13:53:31 +0530

    added home page (final comit)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-bold">
-          <Link to="/">MernHolidays.com</Link>
+          <Link to="/">Holidays.com</Link>
         </span>
         <span className="flex space-x-2">
           {isLoggedIn ? (
